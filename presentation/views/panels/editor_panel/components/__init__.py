68c93f4e92938745aae821a22a7615b9dd99914d
sebasfos196 <sebasfos196@gmail.com>
2025-05-29 15:56:30 +0200

    12

---
+++ presentation/views/panels/editor_panel/components/__init__.py
@@ -0,0 +1,7 @@
+
+# presentation/views/panels/preview_panel/__init__.py
+"""
+Módulo preview_panel - Vista previa.
+"""
+# Por ahora usamos el preview_container original
+# Más adelante se puede cambiar por la versión refactorizada
